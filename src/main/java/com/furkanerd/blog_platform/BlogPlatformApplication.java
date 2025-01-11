7d2aa3d0facc321e2d4da0e1b6f841f08900e01a
furkanerdogan <furkanerdd1@gmail.com>
2025-01-11 19:20:24 +0300

    pom.xml modified and add dotenv support

--- src/main/java/com/furkanerd/blog_platform/BlogPlatformApplication.java
+++ src/main/java/com/furkanerd/blog_platform/BlogPlatformApplication.java
@@ -1,5 +1,6 @@
 package com.furkanerd.blog_platform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogPlatformApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		String dbUrl = dotenv.get("DB_URL");
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+
+		System.setProperty("DB_URL", dbUrl);
+		System.setProperty("DB_USERNAME", dbUsername);
+		System.setProperty("DB_PASSWORD", dbPassword);
 		SpringApplication.run(BlogPlatformApplication.class, args);
 	}
-
 }
